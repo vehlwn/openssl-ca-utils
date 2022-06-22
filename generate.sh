9f0620f859d8dd65c528ca557f8f8787df014b62
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 17:45:17 +0400

    Create folders for ca and site

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "" ]
 then
-    echo "No action given"
+    echo "Usage: ./generate.sh {ca | site}"
     exit 1
 fi
 
@@ -14,10 +14,13 @@ source ./config
 readonly OUT_DIR="out"
 mkdir --parents "${OUT_DIR}"
 
+readonly CA_DIR="${OUT_DIR}/ca"
+
 case "$1" in
   "ca")
     echo "Generating CA key and cert..."
-    cp "ca-req-template.conf" "${OUT_DIR}/ca-req.conf"
+    mkdir --parents "${CA_DIR}"
+    cp "ca-req-template.conf" "${CA_DIR}/ca-req.conf"
     for s in \
         "s/<DEFAULT_BITS>/${CONFIG_DEFAULT_BITS}/g" \
         "s/<DEFAULT_MD>/${CONFIG_DEFAULT_MD}/g" \
@@ -29,15 +32,17 @@ case "$1" in
         "s/<DN_ENAIL>/${CONFIG_DN_ENAIL}/g" \
         "s/<DN_CN>/${CONFIG_DN_O}/g"
     do
-        sed -i "${s}" "${OUT_DIR}/ca-req.conf"
+        sed -i "${s}" "${CA_DIR}/ca-req.conf"
     done
-    openssl req -x509 -new -out "${OUT_DIR}/ca.crt" -keyout "${OUT_DIR}/ca.key" -config "${OUT_DIR}/ca-req.conf" -days "${CONFIG_DAYS}"
-    openssl x509 -in "${OUT_DIR}/ca.crt" -noout -text
+    openssl req -x509 -new -out "${CA_DIR}/ca.crt" -keyout "${CA_DIR}/ca.key" -config "${CA_DIR}/ca-req.conf" -days "${CONFIG_DAYS}"
+    openssl x509 -in "${CA_DIR}/ca.crt" -noout -text
     ;;
 
   "site")
     echo "Generating site key and cert..."
-    cp "site-req-template.conf" "${OUT_DIR}/site-req.conf"
+    readonly SITE_DIR="${OUT_DIR}/${CONFIG_SITE_DNS}"
+    mkdir --parents "${SITE_DIR}"
+    cp "site-req-template.conf" "${SITE_DIR}/site-req.conf"
     for s in \
         "s/<DEFAULT_BITS>/${CONFIG_DEFAULT_BITS}/g" \
         "s/<DEFAULT_MD>/${CONFIG_DEFAULT_MD}/g" \
@@ -50,15 +55,17 @@ case "$1" in
         "s/<DN_CN>/${CONFIG_DN_O}/g" \
         "s/<SITE_DNS>/${CONFIG_SITE_DNS}/g"
     do
-        sed -i "${s}" "${OUT_DIR}/site-req.conf"
+        sed -i "${s}" "${SITE_DIR}/site-req.conf"
     done
-    cp "site-extfile-template.conf" "${OUT_DIR}/site-extfile.conf"
-    sed -i "s/<SITE_DNS>/${CONFIG_SITE_DNS}/g" "${OUT_DIR}/site-extfile.conf"
-    openssl req -new -out "${OUT_DIR}/site.csr" -keyout "${OUT_DIR}/site.key" -config "${OUT_DIR}/site-req.conf"
-    openssl req -in "${OUT_DIR}/site.csr" -noout -text
+    cp "site-extfile-template.conf" "${SITE_DIR}/site-extfile.conf"
+    sed -i "s/<SITE_DNS>/${CONFIG_SITE_DNS}/g" "${SITE_DIR}/site-extfile.conf"
+    openssl req -new -out "${SITE_DIR}/site.csr" -keyout "${SITE_DIR}/key.pem" -config "${SITE_DIR}/site-req.conf"
+    openssl req -in "${SITE_DIR}/site.csr" -noout -text
 
-    openssl x509 -req -in "${OUT_DIR}/site.csr" -CA "${OUT_DIR}/ca.crt" -CAkey "${OUT_DIR}/ca.key" -out "${OUT_DIR}/site.crt" -CAcreateserial -days "${CONFIG_DAYS}" "-${CONFIG_DEFAULT_MD}" -extfile "${OUT_DIR}/site-extfile.conf"
-    openssl x509 -in "${OUT_DIR}/site.crt" -noout -text
+    openssl x509 -req -in "${SITE_DIR}/site.csr" -CA "${CA_DIR}/ca.crt" -CAkey "${CA_DIR}/ca.key" -out "${SITE_DIR}/certificate.pem" -CAcreateserial -days "${CONFIG_DAYS}" "-${CONFIG_DEFAULT_MD}" -extfile "${SITE_DIR}/site-extfile.conf"
+    openssl x509 -in "${SITE_DIR}/certificate.pem" -noout -text
+
+    cat "${SITE_DIR}/certificate.pem" "${CA_DIR}/ca.crt" > "${SITE_DIR}/fullchain.pem"
     ;;
 
   *)
